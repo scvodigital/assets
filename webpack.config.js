744b386eaa6187f43072671dbeee2d2e044c5a3a
Matthew Wilkes <tonicblue@gmail.com>
2018-09-18 13:17:06 +0100

    Allowing for single sites to be built and optional compression

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,38 @@ const autoprefixer = require('autoprefixer');
 const CompressionPlugin = require('compression-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
-module.exports = [
-  getConfig('goodmoves', 'Goodmoves'),
-  getConfig('getinvolved', 'GetInvolved'),
+const sites = [
+  { site: 'goodmoves', library: 'Goodmoves' },
+  { site: 'getinvolved', library: 'GetInvolved' },
 ]
 
-function getConfig(site, library) {
-  return {
+function main() {
+  let toCompile = [];
+  if (process.env.SITES) {
+    const names = process.env.SITES.split(',');
+    sites.forEach(site => {
+      if (names.indexOf(site.name) > -1) {
+        toCompile.push(site);
+      }
+    });
+  }
+  toCompile = toCompile.length > 0 ? toCompile : sites;
+
+  const noCompress = typeof process.env.NO_COMPRESS !== 'undefined' ? Boolean(process.env.NO_COMPRESS) : false;
+
+  console.log('Packing sites:', toCompile);
+  console.log('Not Compressing:', noCompress);
+
+  const modules = toCompile.map(site => {
+    const config = getConfig(site.site, site.library, noCompress);
+    return config;
+  });
+
+  return modules;
+}
+
+function getConfig(site, library, noCompress) {
+  const config = {
     entry: ['./sites/' + site + '/main.scss', './sites/' + site + '/main.js'],
     output: {
       filename: 'build/' + site + '/main.js',
@@ -74,8 +99,11 @@ function getConfig(site, library) {
           },
         }
       ],
-    },
-    plugins: [
+    }
+  };
+
+  if (!noCompress) {
+    config.plugins = [
       new UglifyJsPlugin({
         uglifyOptions: {
           ecma: 5,
@@ -83,6 +111,10 @@ function getConfig(site, library) {
         }
       }),
       new CompressionPlugin(),
-    ],
-  };
+    ];
+  }
+
+  return config;
 }
+
+module.exports = main();

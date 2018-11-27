da4c0843c32d7b44562b419b59d4fde1f278c79c
Matthew Wilkes <tonicblue@gmail.com>
2018-11-27 16:14:09 +0000

    Moved volunteer search into its own repo and symlinked

--- sites/volunteerscotland-search/configuration/queries/notifications/delete-search.sql
+++
@@ -1,9 +0,0 @@
-{{#if @root.data.auth ~}}
-CALL _removeSubscriptionPartition(
-  {{{mysqlEscape @root.data.auth.email}}},
-  {{{mysqlEscape @root.context.metaData.emailCampaignName}}},
-  {{{mysqlEscape @root.request.body.name}}}
-);
-{{else}}
-SET @query=false;
-{{/if ~}}
--- sites/volunteerscotland-search/configuration/queries/notifications/get-saved-searches.sql
+++
@@ -1,9 +0,0 @@
-{{#if @root.data.auth ~}}
-CALL _getSubscriptions(
-  {{{mysqlEscape @root.data.auth.email}}},
-  {{{mysqlEscape @root.context.metaData.emailCampaignName}}},
-  NULL
-);
-{{else}}
-SET @query=false;
-{{/if ~}}

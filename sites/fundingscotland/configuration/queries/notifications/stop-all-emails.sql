e0fa3053cb772bf0c6b5fb6edf85f5e8a45531fb
Matthew Wilkes <tonicblue@gmail.com>
2019-06-03 12:13:45 +0100

    Fixed FS stop-all-emails route

--- sites/fundingscotland/configuration/queries/notifications/stop-all-emails.sql
+++ sites/fundingscotland/configuration/queries/notifications/stop-all-emails.sql
@@ -2,7 +2,11 @@
 UPDATE subscriptionPartitions
 SET active = 0
 WHERE
-  campaign IN ({{{mysqlEscape (concat @root.context.metaData.emailCampaignName '-' @root.data.currentSite.name)}}}, {{{mysqlEscape (concat @root.context.metaData.shortlistCampaignName '-' @root.data.currentSite.name)}}}) AND
+  campaign IN (
+    {{{mysqlEscape (concat @root.context.metaData.emailCampaignName '-' @root.data.currentSite.name)}}},
+    {{{mysqlEscape (concat @root.context.metaData.shortlistCampaignName '-' @root.data.currentSite.name)}}},
+    {{{mysqlEscape (concat @root.context.metaData.bulletinCampaignName '-' @root.data.currentSite.name)}}}
+  ) AND
   email = {{{mysqlEscape @root.data.auth.email}}};
 {{else}}
 SET @query=false;

9ee0e4a70564faddb184c6d668af4621d5192ef3
Calum MacUisdean <calum.macuisdean@gmail.com>
2019-06-17 14:34:01 +0100

    footer styling

--- sites/mpowerhealth/main.js
+++ sites/mpowerhealth/main.js
@@ -1,5 +1,4 @@
 import "@babel/polyfill";
-import { default as Headroom } from 'headroom.js';
 import * as mdc from 'material-components-web';
 import { ComponentsInitialiser } from '../../lib/components-initialiser';
 import { Auth } from '../../lib/firebase-auth';
@@ -51,14 +50,6 @@ export class mPowerHealth {
     this.searchLastStateSelectors = this.searchLastStateArray
         .filter(field => { return field.name !== 'keywords'; })
         .map(field => '[name="' + field.name + '"][value="' + field.value + '"]');
-
-    // Headroom
-    var header = document.querySelector("header.top-bar-stuck");
-    var headroom  = new Headroom(header, {
-      "offset": 138,
-      "tolerance": 5
-    });
-    headroom.init();
 
     const ci = new cookieinfo();
     ci.options.message = "We use cookies to track anonymous usage statistics and do not collect any personal information that can be used to identify you. By continuing to visit this site you agree to our use of cookies.";
